5c901cf4963f7f3a9d3dad83d9c16a99315c4c14
Sufyaan-Onsight <sufyaan@onsightapp.com>
2025-08-22 22:39:59 +0200

    Initial commit of the paywall, ideas to use it as a popup instead of a page

---
+++ src/components/PaywallComponent.vue
@@ -0,0 +1,100 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="paywall-container">
+    <div class="paywall-header">
+      <H1>Paywall</H1>
+      <span>3 x Tickets for Mission Impossible @ R300</span>
+      <button class="close-btn" @click="$emit('close')">X</button>
+    </div>
+
+    <input type="text" class="input-field" placeholder="Cardholder Name">
+    <input type="text" class="input-field" placeholder="Card Number">
+
+    <div class="inline-fields">
+      <input type="date" class="input-field inline-inputs" placeholder="Expiry Date">
+      <input type="text" class="input-field inline-inputs" placeholder="CVV">
+    </div>
+
+    <button class="pay-button">Pay Now</button>
+
+    <div class="card-icons">
+      <img src="https://img.icons8.com/color/48/visa.png" alt="Visa">
+      <img src="https://img.icons8.com/color/48/amex.png" alt="Amex">
+      <img src="https://img.icons8.com/color/48/mastercard.png" alt="MasterCard">
+      <img src="https://img.icons8.com/color/48/discover.png" alt="Discover">
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.paywall-container {
+  border: 2px solid #00FF7F;
+  border-radius: 5px;
+  padding: 20px;
+  width: 80%;
+  height: 50vh;
+}
+
+.paywall-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 2px solid #00FF7F;
+  padding-bottom: 10px;
+  margin-bottom: 20px;
+  font-size: 18px;
+  font-weight: bold;
+}
+
+.input-field {
+  width: 100%;
+  padding: 12px 15px;
+  margin-bottom: 15px;
+  border-radius: 25px;
+  border: none;
+  background: white;
+  color: #1a1a1a;
+}
+
+.inline-inputs{
+  width: 10vw;
+}
+
+.inline-fields {
+  display: flex;
+  gap: 10px;
+}
+
+.inline-fields input {
+  flex: 1;
+}
+
+.pay-button {
+  width: 50%;
+  padding: 15px;
+  border-radius: 25px;
+  background: #00FF7F;
+  color: black;
+  font-weight: bold;
+  transition: 0.3s;
+}
+
+.pay-button:hover {
+  background: #00cc66;
+}
+
+.card-icons {
+  display: flex;
+  justify-content: center;
+  gap: 15px;
+  margin-top: 15px;
+}
+
+.card-icons img {
+  width: 50px;
+}
+</style>
---
+++ src/pages/PaywallPage.vue
@@ -0,0 +1,28 @@
+<script>
+import paywallComponent from "../components/PaywallComponent.vue";
+
+export default {
+  components: { paywallComponent },
+  data() {
+    return {
+      showPopup: false,
+    };
+  },
+};
+</script>
+
+<template>
+  <div>
+    <button @click="showPopup = true" class="open-btn">Open Popup</button>
+    <paywall-component v-if="showPopup" @close="showPopup = false"/>
+  </div>
+</template>
+
+<style>
+.open-btn {
+  padding: 10px 20px;
+  background: #00ff80;
+  border: none;
+  border-radius: 8px;
+}
+</style>
